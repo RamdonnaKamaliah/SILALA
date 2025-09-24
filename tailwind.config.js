c592a4b0789585520d246c5d533839875c9200f8
eliyanur <eliyanurarrahmadan>
2025-09-24 14:07:59 +0700

    membenarkan kesalahan

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        '../../vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        '../../storage/framework/views/*.php',
+        '../**/*.blade.php',
+        '../**/*.js',
     ],
 
     theme: {

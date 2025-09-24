61938773cc29c4f3a66a5c979a05babec0e004ef
eliyanur <eliyanurarrahmadan>
2025-09-24 15:32:46 +0700

    ini membetulkan konflik

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,6 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        '../../vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        '../../storage/framework/views/*.php',
-        '../**/*.blade.php',
-        '../**/*.js',
     ],
 
     theme: {

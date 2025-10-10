5d3907c2b12e42fbe8f63eb1698a31e0042d1378
Rifdah <email_rifdah@domain.com>
2025-10-10 10:41:20 +0700

    Update Fitur: Data Kategori

---
+++ app/Models/datakategori.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class datakategori extends Model
+{
+    protected $table = 'data_kategoris';
+    protected $fillable = ['nama_kategori'];
+}
